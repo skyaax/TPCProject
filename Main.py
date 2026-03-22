86a581c37db3f7c1ee25dac109ee6599025aee63
skyaax <skajrem@gmail.com>
2026-03-22 13:49:13 +0100

    started the project

---
+++ Main.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+def addition(matrix1, matrix2):
+    matrix3 = np.empty(matrix1.shape)
+    rows,cols = matrix1.shape
+    for i in range(rows):
+        for j in range(cols):
+            matrix3[i][j] = matrix1[i][j] + matrix2[i][j]
+    return matrix3
+
+
+def multiplication(matrix1, matrix2):
+    matrix3 = np.empty(matrix1.shape)
+    rows, cols = matrix1.shape
+
+    posy = 0
+    for i in range(rows):
+        for j in range(cols):
+            matrix3[i][posy] += matrix1[i][j] * matrix2[j][i]
+        posy += 1
+
+
+    return matrix3
+
+
+
+
+
+if __name__ == '__main__':
+    matrix1 = np.array([[1, 2], [4, 5]])
+    matrix2 = np.array([[10, 10], [10, 10]])
+
+    multiplication(matrix1, matrix2)
+
+
+
